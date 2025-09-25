72ba0e52fcf74ab8cc36e2dea0eb61e8c52c29ec
Bash Elliott <bashelliott@gmail.com>
2025-09-25 11:08:15 +1000

    a little bit of ui setup

--- reggie/__main__.py
+++ reggie/__main__.py
@@ -5,30 +5,45 @@ class Reggie:
 		'''
 		Reggie. Text-based IRC client (hopefully)
 		'''
-		self.height = shutil.get_terminal_size()[1] - 1
+		self.width = shutil.get_terminal_size()[0]
+		self.height = shutil.get_terminal_size()[1] - 2
 		self.lines = []
 		self.offset = 0
 		self.latched = True
-		self.cnt = 0
 		self.fps = 30
+
+		self.appendToLog(open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'splash.txt'),'rb').readlines())
+
+		self.cnt = 0
+
+	def appendToLog(self, feed):
+		self.lines += [line.decode('unicode-escape').strip('\n') for line in feed]
 
 	def draw(self): # Render the lines.
 		while True:
-			os.system('cls' if os.system == 'nt' else 'clear')
+			sys.stdout.write("\033[H\033[J")
 
 			for i in range(0, self.height):
 				try:
 					print(self.lines[i + self.offset])
 				except IndexError:
 					print("")
-			print(self.latched, end='')
+			sys.stdout.write("-" * self.width)
+
+			statusLeft = f"{self.offset}/{len(self.lines)}"
+			statusRight = "Latched" if self.latched else "Unlatched"
+
+			space_count = max(0, self.width - len(statusLeft) - len(statusRight))
+			statusBar = statusLeft + (" " * space_count) + statusRight
+
+			sys.stdout.write(statusBar)
+			sys.stdout.flush()
 			time.sleep(1/self.fps)
 
 	def KeyEventManager(self): # 
 		while True:
 			try:
 				key = readchar.readkey()
-				print(key)
 				if key == readchar.key.UP:
 					self.offset -= 1
 					if self.latched:
@@ -36,10 +51,13 @@ class Reggie:
 					if self.offset < 0:
 						self.offset = 0
 				elif key == readchar.key.DOWN:
-					self.offset += 1
-					if self.offset > len(self.lines) - self.height:
-						self.offset = len(self.lines) - self.height
-						self.latched = True
+					if self.latched:
+						pass
+					else:
+						self.offset += 1
+						if self.offset > len(self.lines) - self.height:
+							self.offset = len(self.lines) - self.height
+							self.latched = True
 			except AttributeError:
 				pass
 
@@ -50,22 +68,22 @@ class Reggie:
 			self.cnt += 1
 
 			if self.latched:
-				self.offset = len(self.lines) - self.height
+				self.offset = len(self.lines) - self.height + 1
 				if self.offset < 0:
 					self.offset = 0
 
 def main():
 	reg = Reggie()
 
-	adder = threading.Thread(
-		target=reg.increment,
-		name="Incrementer")
-	adder.start()
-
 	drawer = threading.Thread(
 		target=reg.draw,
 		name="Renderer")
 	drawer.start()
+
+	adder = threading.Thread(
+		target=reg.increment,
+		name="Incrementer")
+	adder.start()
 
 	input = threading.Thread(
 		target=reg.KeyEventManager,
